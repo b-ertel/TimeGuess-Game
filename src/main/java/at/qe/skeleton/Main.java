138ce43ca45ee48398b7d1f3ccede33f5696790a
csap4266 <alexander.blaas@student.uibk.ac.at>
2021-02-16 16:46:46 +0100

    first draft with websockets introduced

--- src/main/java/at/qe/skeleton/Main.java
+++ src/main/java/at/qe/skeleton/Main.java
@@ -1,10 +1,10 @@
 package at.qe.skeleton;
 
-import at.qe.skeleton.configs.CustomServletContextInitializer;
-import at.qe.skeleton.configs.WebSecurityConfig;
-import at.qe.skeleton.utils.ViewScope;
 import java.util.HashMap;
+
 import javax.faces.webapp.FacesServlet;
+
+import org.apache.catalina.startup.ContextConfig;
 import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,7 +12,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+import at.qe.skeleton.configs.CustomServletContextInitializer;
+import at.qe.skeleton.configs.WebSecurityConfig;
+import at.qe.skeleton.utils.ViewScope;
 
 /**
  * Spring boot application. Execute maven with <code>mvn spring-boot:run</code>
@@ -24,34 +31,45 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
  */
 @SpringBootApplication
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+/*
+ * Prevent spring from trying to autowire the websocket-infrastructure: Exclude
+ * the at.qe.skeleton.ui.websockets package from component scan.
+ *
+ * NOTE: Do not add any components to this package which should be managed by
+ * spring. It is reserved for the CDI-injection-mechanisms (Weld). Only add
+ * CDI-managed components.
+ */
+@ComponentScan(basePackages = "at.qe.skeleton", excludeFilters = @Filter(type = FilterType.REGEX, pattern = "at.qe.skeleton.ui.websockets.*"))
 public class Main extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(new Class[]{Main.class, CustomServletContextInitializer.class, WebSecurityConfig.class});
-    }
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Main.class, CustomServletContextInitializer.class, WebSecurityConfig.class,
+				ContextConfig.class);
+	}
 
-    @Bean
-    public ServletRegistrationBean servletRegistrationBean() {
-        FacesServlet servlet = new FacesServlet();
-        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(servlet, "*.xhtml");
-        servletRegistrationBean.setName("Faces Servlet");
-        servletRegistrationBean.setAsyncSupported(true);
-        servletRegistrationBean.setLoadOnStartup(1);
-        return servletRegistrationBean;
-    }
+	@Bean
+	public ServletRegistrationBean<FacesServlet> servletRegistrationBean() {
+		FacesServlet servlet = new FacesServlet();
+		ServletRegistrationBean<FacesServlet> servletRegistrationBean = new ServletRegistrationBean<>(servlet,
+				"*.xhtml");
+		servletRegistrationBean.setName("Faces Servlet");
+		servletRegistrationBean.setAsyncSupported(true);
+		servletRegistrationBean.setLoadOnStartup(1);
+		return servletRegistrationBean;
+	}
 
-    @Bean
-    public CustomScopeConfigurer customScopeConfigurer() {
-        CustomScopeConfigurer customScopeConfigurer = new CustomScopeConfigurer();
-        HashMap<String, Object> customScopes = new HashMap<>();
-        customScopes.put("view", new ViewScope());
-        customScopeConfigurer.setScopes(customScopes);
-        return customScopeConfigurer;
-    }
+	@Bean
+	public CustomScopeConfigurer customScopeConfigurer() {
+		CustomScopeConfigurer customScopeConfigurer = new CustomScopeConfigurer();
+		HashMap<String, Object> customScopes = new HashMap<>();
+		customScopes.put("view", new ViewScope());
+		customScopeConfigurer.setScopes(customScopes);
+		return customScopeConfigurer;
+	}
 
 }
