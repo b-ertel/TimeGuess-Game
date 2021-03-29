0927689f85fc6f88dfbdade22b60621960f656d0
Clemens Ager <clemens.ager@student.uibk.ac.at>
2021-03-29 19:11:33 +0200

    add service

---
+++ backend/src/main/java/at/timeguess/backend/services/GameService.java
@@ -0,0 +1,41 @@
+package at.timeguess.backend.services;
+
+import java.util.Collection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Component;
+
+import at.timeguess.backend.repositories.GameRepository;
+import at.timeguess.backend.model.*;
+
+@Component
+@Scope("application")
+public class GameService {
+	@Autowired
+    private GameRepository gameRepo;
+	
+   
+    @PreAuthorize("hasAuthority('USER')")
+    public Collection<Game> getAllGames() {
+        return gameRepo.findAll();
+        }
+    @PreAuthorize("hasAuthority('MANAGER')")
+    public Collection<Game> getByState(GameState gs){
+    	return gameRepo.findByState(gs);
+    }
+    
+    @PreAuthorize("hasAuthority('MANAGER') ")
+    public Game loadGame(Long gameId) {
+        return gameRepo.findById(gameId);
+    }
+    @PreAuthorize("hasAuthority('MANAGER')")
+    public Game saveGame(Game g) {
+      // TODO: confirm validity
+        return gameRepo.save(g);
+    }
+    @PreAuthorize("hasAuthority('MANAGER')")
+    public void deleteGame(Game g) {
+        gameRepo.delete(g);
+    }
+}
