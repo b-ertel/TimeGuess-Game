b6f5f8f181e72549050674c3f3f31d5524148a2a
Clemens Ager <clemens.ager@student.uibk.ac.at>
2021-03-30 18:50:11 +0200

    Comments to gameState

--- backend/src/main/java/at/timeguess/backend/model/Game.java
+++ backend/src/main/java/at/timeguess/backend/model/Game.java
@@ -1,5 +1,6 @@
 package at.timeguess.backend.model;
 
+import java.io.Serializable;
 import java.util.Set;
 
 import javax.persistence.Column;
@@ -10,6 +11,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 
 import org.springframework.data.domain.Persistable;
 
@@ -17,92 +19,118 @@ import at.timeguess.backend.model.game.GameException;
 import at.timeguess.backend.model.game.GameState;
 
 @Entity
-public class Game implements Persistable<Long> {
+public class Game implements Persistable<Long>, Serializable {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 7796168379870118060L;
 
-	@Column
-	private String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	private int maxPoints;
+    @Column
+    private String name;
 
-	private boolean status;
-	private GameState state;
+    private int maxPoints;
 
-	
+    private boolean status;
+    private GameState state;
 
-	private int roundNr;
+    private int roundNr;
 
-	@ManyToMany
-	@JoinTable(name = "game_team", joinColumns = @JoinColumn(name = "game_id"), inverseJoinColumns = @JoinColumn(name = "team_id"))
-	private Set<Team> teams;
+    @ManyToMany
+    @JoinTable(name = "game_team", joinColumns = @JoinColumn(name = "game_id"), inverseJoinColumns = @JoinColumn(name = "team_id"))
+    private Set<Team> teams;
 
-	public Long getId() {
-		return id;
-	}
+    @ManyToOne
+    private Topic topic;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public int getMaxPoints() {
-		return maxPoints;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setMaxPoints(int maxPoints) {
-		this.maxPoints = maxPoints;
-	}
+    public int getMaxPoints() {
+        return maxPoints;
+    }
 
-	public boolean isStatus() {
-		return status;
-	}
+    public void setMaxPoints(int maxPoints) {
+        this.maxPoints = maxPoints;
+    }
 
-	public void setStatus(boolean status) {
-		this.status = status;
-	}
+    public boolean isStatus() {
+        return status;
+    }
 
-	public int getRoundNr() {
-		return roundNr;
-	}
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 
-	public void setRoundNr(int roundNr) {
-		this.roundNr = roundNr;
-	}
+    public int getRoundNr() {
+        return roundNr;
+    }
 
-	public Set<Team> getTeams() {
-		return teams;
-	}
+    public void setRoundNr(int roundNr) {
+        this.roundNr = roundNr;
+    }
 
-	public void setTeams(Set<Team> teams) {
-		this.teams = teams;
-	}
+    public Set<Team> getTeams() {
+        return teams;
+    }
 
-	public GameState getState() {
-		return state;
-	}
+    public void setTeams(Set<Team> teams) {
+        this.teams = teams;
+    }
 
-	public void setState(GameState state) {
-		this.state = state;
-	}
+    /**
+     * @return the topic
+     */
+    public Topic getTopic() {
+        return topic;
+    }
 
-	public void advanceState(GameState state) throws GameException {
-		// TODO - check if transition is allowed 
-		this.state = state;
-	}
+    /**
+     * @param topic the topic to set
+     */
+    public void setTopic(Topic topic) {
+        this.topic = topic;
+    }
 
-	@Override
-	public boolean isNew() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    public GameState getState() {
+        return state;
+    }
+
+    public void setState(GameState state) {
+        this.state = state;
+    }
+
+    public void advanceState(GameState state) throws GameException {
+        // TODO - check if transition is allowed
+        this.state = state;
+    }
+
+    public boolean canBePlayed() {
+        // TODO
+        // check for number teams, teams sufficient players
+        return false;
+    }
+
+    @Override
+    public boolean isNew() {
+        // TODO Auto-generated method stub
+        return false;
+    }
 }
--- backend/src/main/java/at/timeguess/backend/model/game/GameException.java
+++ backend/src/main/java/at/timeguess/backend/model/game/GameException.java
@@ -1,4 +1,9 @@
 package at.timeguess.backend.model.game;
 
+
+/**
+ * For future use, throwing exceptions when invalid Game configuration is tried to be used.
+ *
+ */
 public class GameException extends Exception {
 }
