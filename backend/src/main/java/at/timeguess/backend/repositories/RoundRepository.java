506a7650e1984c95e0d21866deab1588e889d185
Michael Sonnerer <michael.sonnerer@student.uibk.ac.at>
2021-04-01 12:31:58 +0200

    layout for lobby stats

--- backend/src/main/java/at/timeguess/backend/repositories/RoundRepository.java
+++ backend/src/main/java/at/timeguess/backend/repositories/RoundRepository.java
@@ -14,10 +14,10 @@ public interface RoundRepository extends AbstractRepository<Round, Long> {
 	
 	//TODO: Query to find Terms
 	
-	@Query("SELECT COUNT(topic) FROM Round r JOIN Term t WHERE r.termToGuess = t.id AND r.correctAnswer=true AND topic=:topicid")
+	@Query("SELECT COUNT(topic) FROM Round r JOIN Term t ON (r.termToGuess = t.id) WHERE r.correctAnswer=true AND topic=:topicid")
 	int getNrCorrectAnswerByTopic(@Param("topicid") long topicid);
 	
-	@Query("SELECT COUNT(topic) FROM Round r JOIN Term t WHERE r.termToGuess = t.id AND r.correctAnswer=false AND topic=:topicid")
+	@Query("SELECT COUNT(topic) FROM Round r JOIN Term t ON (r.termToGuess = t.id) WHERE r.correctAnswer=false AND topic=:topicid")
 	int getNrIncorrectAnswerByTopic(@Param("topicid") long topicid);
 	//TODO: Query to find correct Answers from certain User
 }
