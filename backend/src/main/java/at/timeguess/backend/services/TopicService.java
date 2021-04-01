e1d207eb3a14e65bd0e939fe890bbcd3b85f59d2
Felix Tschimben <felix.tschimben@student.uibk.ac.at>
2021-04-01 15:35:15 +0200

    added Term and Topic Services

---
+++ backend/src/main/java/at/timeguess/backend/services/TopicService.java
@@ -0,0 +1,62 @@
+package at.timeguess.backend.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Component;
+
+import at.timeguess.backend.model.Topic;
+import at.timeguess.backend.model.Exceptions.TopicAlreadyExistsException;
+import at.timeguess.backend.repositories.TopicRepository;
+
+/**
+ * Provides an interface to the model for managing {@link Topic} entities.
+ */
+@Component
+@Scope("application")
+public class TopicService {
+    
+    @Autowired
+    private TopicRepository topicRepository;
+
+    /**
+     * Returns a list of all topics.
+     *
+     * @return list of all topics
+     */
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public List<Topic> getAllTopics() {
+        return topicRepository.findAll();
+    }
+
+    /**
+     * Loads a single Topic identified by it's name.
+     *
+     * @param name the name of the topic to load
+     * @return a single Topic
+     */
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public Topic loadTopic(String name) {
+        return topicRepository.findByName(name);
+    }
+
+    /**
+     * Saves the Topic. 
+     *
+     * @param topic the topic to save
+     * @return the new topic
+     * @throws TopicAlreadyExistsException
+     */
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public Topic saveTopic(Topic topic) throws TopicAlreadyExistsException {
+        if (topic.isNew()) {
+            Topic newTopic = topicRepository.save(topic);
+            return newTopic;
+        } else {
+            throw new TopicAlreadyExistsException();
+        }
+    }
+    
+}
