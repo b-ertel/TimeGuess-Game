4795604db900eadba4ecdf22a303dfcce894eba7
Clemens Ager <clemens.ager@student.uibk.ac.at>
2021-04-05 17:03:56 +0200

    Game Repository

---
+++ backend/src/main/java/at/timeguess/backend/repositories/GameRepository.java
@@ -0,0 +1,22 @@
+package at.timeguess.backend.repositories;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import at.timeguess.backend.model.Game;
+import at.timeguess.backend.model.GameState;
+
+public interface GameRepository extends AbstractRepository<Game, Long> {
+
+    /**
+     * to find open games (GameState.VALID_SETUP), active (GameState.PLAYED),
+     * finished (GameState.FINISHED)
+     *
+     * @param status
+     * @return
+     */
+    @Query("SELECT g FROM Game g WHERE :status =  g.status")
+    Collection<Game> findByState(@Param("status") GameState status);
+}
