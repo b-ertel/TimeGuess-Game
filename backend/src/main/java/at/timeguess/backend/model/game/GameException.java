8744bd19ae4e226b53088bab4ad3fff977be471e
Clemens Ager <clemens.ager@student.uibk.ac.at>
2021-04-05 17:12:22 +0200

    cleanup

--- backend/src/main/java/at/timeguess/backend/model/game/GameException.java
+++
@@ -1,9 +0,0 @@
-package at.timeguess.backend.model.game;
-
-
-/**
- * For future use, throwing exceptions when invalid Game configuration is tried to be used.
- *
- */
-public class GameException extends Exception {
-}
