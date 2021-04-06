c9672afac575e70df4a9e5139978ac0253a930a3
Michael Sonnerer <michael.sonnerer@student.uibk.ac.at>
2021-04-06 19:03:33 +0200

    Resolve merge conflicts

--- backend/src/main/java/at/timeguess/backend/repositories/GameRepository.java
+++ backend/src/main/java/at/timeguess/backend/repositories/GameRepository.java
@@ -12,4 +12,18 @@ public interface GameRepository extends AbstractRepository<Game, Long> {
 	
 	@Query("SELECT g.topic, COUNT(g.topic) AS occ FROM Game g GROUP BY g.topic ORDER BY occ DESC")
 	List<Object[]> getTopicAndOccurency();
+	
+	@Query("SELECT new GroupingHelper(g.game.id, MAX(g.points)) FROM GameTeam g GROUP BY g.game.id")
+    List<GroupingHelper> findMaxPointsReached();
+
+    @Query("SELECT new GroupingHelper(o.team.id, COUNT(*)) FROM GameTeam o WHERE o.points < (SELECT MAX(i.points) FROM GameTeam i WHERE i.game=o.game GROUP BY i.game) GROUP BY o.team.id")
+    List<GroupingHelper> findLoserTeams();
+
+    @Query("SELECT new GroupingHelper(o.team.id, COUNT(*)) FROM GameTeam o WHERE (o.game.id, o.points) IN (SELECT i.game.id, MAX(i.points) FROM GameTeam i GROUP BY i.game.id) GROUP BY o.team.id")
+    List<GroupingHelper> findWinnerTeams();
+
+    @Query("SELECT new GroupingHelper(o.team.id, g.topic.name, COUNT(*)) FROM GameTeam o JOIN Game g ON g.id=o.game.id WHERE (o.game.id, o.points) IN (SELECT i.game.id, MAX(i.points) FROM GameTeam i GROUP BY i.game.id) GROUP BY o.team.id, g.topic.name")
+    List<GroupingHelper> findWinnerTeamsByTopic();
+
+
 }
