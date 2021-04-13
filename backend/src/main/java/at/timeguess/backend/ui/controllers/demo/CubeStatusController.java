1cb51c6b0abccfc52e3e0a58faf2a08fe3215a28
Verena Fritz (c7031304) <verena.fritz@uibk.ac.at>
2021-04-13 19:39:12 +0200

    add CubeStatusController.java and cubeStatusOutput.xhtml and modify cubeTest.xhtml

---
+++ backend/src/main/java/at/timeguess/backend/ui/controllers/demo/CubeStatusController.java
@@ -0,0 +1,82 @@
+package at.timeguess.backend.ui.controllers.demo;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import at.timeguess.backend.model.Cube;
+import at.timeguess.backend.model.User;
+import at.timeguess.backend.model.demo.LogEntry;
+import at.timeguess.backend.model.demo.LogEntryType;
+import at.timeguess.backend.model.demo.UserStatus;
+import at.timeguess.backend.model.demo.UserStatusInfo;
+import at.timeguess.backend.repositories.CubeRepository;
+import at.timeguess.backend.repositories.UserRepository;
+import at.timeguess.backend.spring.UserStatusInitializationHandler;
+import at.timeguess.backend.ui.websockets.WebSocketManager;
+import at.timeguess.backend.utils.CDIAutowired;
+import at.timeguess.backend.utils.CDIContextRelated;
+
+/**
+ * This controller holds and manages all user's status-information (i.e. their
+ * online-status)
+ *
+ * This class is part of the skeleton project provided for students of the
+ * courses "Software Architecture" and "Software Engineering" offered by the
+ * University of Innsbruck.
+ */
+@Controller
+@Scope("application")
+@CDIContextRelated
+public class CubeStatusController {
+
+    @Autowired
+    private CubeRepository cubeRepository;
+    @CDIAutowired
+    private WebSocketManager websocketManager;
+    private Map<String, Cube> cubeStatus = new ConcurrentHashMap<>();
+//    private List<LogEntry> actionLogs = new CopyOnWriteArrayList<>();
+
+    /**
+     * Called by the {@link UserStatusInitializationHandler}: When the
+     * database-connection is established, all users can be retrieved and the
+     * collection holding the user-status can be setup.
+     *
+     * NOTE: The {@link UserStatusInitializationHandler} calls this setup-method
+     * only once at startup. If you add any other users, they will not appear within
+     * the user-status-collection. You either need to restart the application
+     * (feasible in development-mode, really bad behavior in production) or call
+     * this setup.method again to refresh the mentioned collection
+     */
+    public void setupCubeStatus() {
+        this.cubeRepository.findAll()
+                .forEach(cube -> this.cubeStatus.put(cube.getMacAddress(), cube));
+    }
+
+
+    /**
+     * Simply appends a log-entry to the action-log.
+     *
+     * @param user
+     * @param type
+     */
+    /*   private void log(User user, LogEntryType type) {
+        this.actionLogs.add(new LogEntry(user, type));
+    }
+*/
+    public Collection<Cube> getCubeStatusInfos() {
+        return Collections.unmodifiableCollection(this.cubeStatus.values());
+    }
+/*
+    public List<LogEntry> getActionLogs() {
+        return Collections.unmodifiableList(this.actionLogs);
+    }
+*/
+}
