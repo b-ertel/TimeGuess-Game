438407b307a6c1dc10bbcd0c75717d467de089c1
Bernhard Ertel <bernhard.ertel@student.uibk.ac.at>
2021-04-14 22:13:33 +0200

    added WebSocketCubeFace.java added CubeFace in webSocketRoom.xhtml
    todo: documentation

--- backend/src/main/java/at/timeguess/backend/ui/controllers/demo/WebSocketTermController.java
+++ backend/src/main/java/at/timeguess/backend/ui/controllers/demo/WebSocketTermController.java
@@ -1,29 +1,16 @@
 package at.timeguess.backend.ui.controllers.demo;
 
 import at.timeguess.backend.model.Term;
-import at.timeguess.backend.model.User;
-import at.timeguess.backend.model.demo.LogEntry;
-import at.timeguess.backend.model.demo.LogEntryType;
-import at.timeguess.backend.model.demo.UserStatus;
-import at.timeguess.backend.model.demo.UserStatusInfo;
 import at.timeguess.backend.repositories.UserRepository;
 import at.timeguess.backend.services.TermService;
-import at.timeguess.backend.spring.UserStatusInitializationHandler;
-import at.timeguess.backend.ui.controllers.TermListController;
 import at.timeguess.backend.ui.websockets.WebSocketManager;
 import at.timeguess.backend.utils.CDIAutowired;
 import at.timeguess.backend.utils.CDIContextRelated;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
-
 import javax.annotation.PostConstruct;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
+
 
 /**
  * This controller is responsible for showing a term in the game window with websockets
@@ -37,7 +24,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 @Controller
 @Scope("application")
 @CDIContextRelated
-public class termController {
+public class WebSocketTermController {
 
     @Autowired
     private UserRepository userRepository;
