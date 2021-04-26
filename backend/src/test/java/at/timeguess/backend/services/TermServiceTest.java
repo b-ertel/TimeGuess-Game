f1020fb3ad80eeedabc876cf30c38c3d61764204
Bernhard Ertel <bernhard.ertel@student.uibk.ac.at>
2021-04-26 08:57:53 +0200

    added tests to TermServiceTest

--- backend/src/test/java/at/timeguess/backend/services/TermServiceTest.java
+++ backend/src/test/java/at/timeguess/backend/services/TermServiceTest.java
@@ -1,7 +1,8 @@
 package at.timeguess.backend.services;
 
-import org.junit.Assert;
+import at.timeguess.backend.model.Term;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -15,9 +16,18 @@ public class TermServiceTest {
     @Autowired
     TermService termService;
 
+    @Autowired
+    TopicService topicService;
+
     @Test
-    public void testDataInitialization(){
-        Assert.assertEquals("Insufficient amount of terms initialized for test data source", 20, termService.getAllTerms().size());
+    public void testDatainitialization(){
+        Assertions.assertTrue( termService.getAllTerms().size() >= 19, "Insufficient amount of terms initialized for test data source");
+        for (Term term : termService.getAllTerms()) {
+            if (term.getId() == 1){
+                Assertions.assertEquals(  "AFRICA", term.getName(),"Term \"1\" does not have the initialized name");
+                Assertions.assertEquals(  topicService.getAllTopics().get(1), term.getTopic(),"Term \"1\" is not in topic \"1\" as initialized");
+            }
+        }
     }
 
 }
