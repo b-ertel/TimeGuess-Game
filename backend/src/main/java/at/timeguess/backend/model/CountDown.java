8d573196e985018941acfb85e420b665023115ff
Verena Fritz (c7031304) <verena.fritz@uibk.ac.at>
2021-04-28 20:47:21 +0200

    countdown works but cannot be stopped

--- backend/src/main/java/at/timeguess/backend/model/CountDown.java
+++ backend/src/main/java/at/timeguess/backend/model/CountDown.java
@@ -1,5 +1,8 @@
 package at.timeguess.backend.model;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class CountDown {
 
 	private int minutes;
@@ -8,6 +11,8 @@ public class CountDown {
 	public CountDown() {
 		setSeconds(0);
 	}
+	
+
 	
 	public CountDown(int minutes) {
 		setMinutes(minutes);
@@ -34,6 +39,62 @@ public class CountDown {
 	public String toString() {
 		return getMinutes() + " : " +  getSeconds();
 	}
+	/*
+	private int sec;
+	private int min;
+
+	public CountDown(int min, int sec) {
+		Timer timer = new Timer();
+		this.min = min;
+		this.sec = sec;
+		TimerTask task = new MyTimerTask();
 	
+		timer.schedule(task, 0, 1000);
+
+	}
 	
+	public class MyTimerTask extends TimerTask {
+	
+		@Override
+		public void run() {
+			System.out.println(getMin() + " : " + getSec());
+			
+			if (min >= 0) {
+				if(sec > 0) {
+					System.out.println(sec);
+					setSec(getSec()-1);
+				}
+			}
+			if(sec==0 && min > 0)
+				min--;
+				System.out.println("ha");
+				sec = 60;
+			
+
+			if (min == 0 && sec == 0)
+				System.exit(0);
+		}
+			
+	}
+	
+	public int getSec() {
+		return sec;
+	}
+
+	public void setSec(int sec) {
+		this.sec = sec;
+	}
+
+	public int getMin() {
+		return min;
+	}
+
+	public void setMin(int min) {
+		this.min = min;
+	}
+
+	public static void main(String[] args) {
+		new CountDown(1, 0);
+	}	*/
 }
+	
--- backend/src/main/java/at/timeguess/backend/ui/controllers/CountDownController.java
+++ backend/src/main/java/at/timeguess/backend/ui/controllers/CountDownController.java
@@ -1,9 +1,8 @@
 package at.timeguess.backend.ui.controllers;
 
 import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 
-import at.timeguess.backend.model.CountDown;
 import at.timeguess.backend.model.Cube;
 import at.timeguess.backend.ui.websockets.WebSocketManager;
 import at.timeguess.backend.utils.CDIAutowired;
@@ -13,53 +12,93 @@ import at.timeguess.backend.utils.CDIContextRelated;
  *  Controller for displaying {@link Cube} entities.
  *
  */
-@Component
+@Controller
 @CDIContextRelated
-@Scope("session")
+@Scope("application")
 public class CountDownController {
 
-	private CountDown countDown;
-	private int minutes;
+	private int min;
+	private int sec;
+	private boolean count;
 	
     @CDIAutowired
 	private WebSocketManager webSocketManager;
 	
-	public void startCountdown(int minutes) {
-		this.countDown = new CountDown();
-		this.countDown.setMinutes(minutes);
+	public void startCountdown(int min, int sec) {
+		this.min = min;
+		this.sec= sec;
+		run();
+		System.out.println(getCountDown());
 		count();
 	}
 	
 	public void count() {
-		while(minutes != 0) {
-			System.out.println(countDown);
-			minutes--;
-			this.countDown.setMinutes(minutes);
-			webSocketManager.getCubeChannel().send("countDownUpdate");
-		}
+		
+			while(min >= 0) {
+				while(sec > 0) {
+					if(count == false) {
+						break;
+					}
+					sec--;
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					webSocketManager.getCountDownChannel().send("countDownUpdate");
+				
+					if(min==0) {
+						break;
+					}
+				}
+				min--;
+				sec = 60;
+			}
+
+	}
+	
+	
+	public void setCountDown(int min, int sec){
+		setMin(min);
+		setSec(sec);
+	}
+	
+	
+	public void run() {
+		this.count=true;
+	}
+	
+	public void stop() {
+		this.count=false;
+	}
+	
+	public String getCountDown() {
+		return getMin() + " : " + getSec();
 	}
 
-	
+	/*
 	public static void main(String args[]) {
 		System.out.println("Does this work?");
 		CountDownController controller = new CountDownController();
-		controller.startCountdown(60);
+		controller.startCountdown(1, 0);
+	}
+*/
+
+	public int getMin() {
+		return min;
 	}
 
-	public CountDown getCountDown() {
-		return countDown;
+	public void setMin(int min) {
+		this.min = min;
 	}
 
-	public void setCountDown(CountDown countDown) {
-		this.countDown = countDown;
+	public int getSec() {
+		return sec;
 	}
 
-	public int getMinutes() {
-		return minutes;
-	}
-
-	public void setMinutes(int minutes) {
-		this.minutes = minutes;
+	public void setSec(int sec) {
+		this.sec = sec;
 	}
 
 }
