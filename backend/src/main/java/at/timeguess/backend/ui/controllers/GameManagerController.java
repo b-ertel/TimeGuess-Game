9848feaa9864d32a11edf990ad2fef6e9cc82f1f
Claudia Wagner (csaw9017) <claudia.wagner@student.uibk.ac.at>
2021-06-03 20:49:59 +0200

    - change meaning of gamestates slightly to make halting a game from outside possible - move most css from gameroom to external file

--- backend/src/main/java/at/timeguess/backend/ui/controllers/GameManagerController.java
+++ backend/src/main/java/at/timeguess/backend/ui/controllers/GameManagerController.java
@@ -59,13 +59,14 @@ public class GameManagerController {
 
     // cannot implement two different Consumers!
     private Consumer<ConfiguredFacetsEvent> consumerConfiguredFacetsEvent =
-            (cfEvent) -> GameManagerController.this.onConfiguredFacetsEvent(cfEvent);
+        (cfEvent) -> GameManagerController.this.onConfiguredFacetsEvent(cfEvent);
     private Consumer<ChannelPresenceEvent> consumerChannelPresenceEvent =
-            (cpEvent) -> GameManagerController.this.onChannelPresenceEvent(cpEvent);
+        (cpEvent) -> GameManagerController.this.onChannelPresenceEvent(cpEvent);
 
     private Map<Game, Round> currentRound = new ConcurrentHashMap<>();
     private Map<Game, Boolean> midValidation = new ConcurrentHashMap<>();
     private Map<Game, Boolean> activeRound = new ConcurrentHashMap<>(); // indicates if a round is currently played in a certain game
+    private Map<Game, Integer> presentTeams = new ConcurrentHashMap<>();
     private Map<Cube, Game> listOfGames = new ConcurrentHashMap<>();
 
     @PostConstruct
@@ -81,33 +82,33 @@ public class GameManagerController {
     }
 
     /**
-     * A method for processing a {@link ConfiguredFacetsEvent}. Method is called on facet-event, checks to which game
-     * the event belongs and estimates whether a round should start or end
+     * A method for processing a {@link ConfiguredFacetsEvent}.
+     * Method is called on facet-event, checks to which game the event belongs and estimates whether a round should start or end.
      */
     public synchronized void onConfiguredFacetsEvent(ConfiguredFacetsEvent configuredFacetsEvent) {
         if (listOfGames.keySet().contains(configuredFacetsEvent.getCube())) {
             Game game = listOfGames.get(configuredFacetsEvent.getCube());
 
-            if(game.getStatus().equals(GameState.PLAYED)) {   // checks if game is in state PLAYED otherwise there should not be started a new round
-            	if(!midValidation.get(game)) {
-            		if (!activeRound.get(game)) {
-            			activeRound.put(game, true);
-            			startRoundByCube(game, currentRound.get(game), configuredFacetsEvent.getCubeFace());
-            		}
-            		else {
-                    	activeRound.put(game, false);
-                    	midValidation.put(game, true);
-                    	this.websocketManager.getNewRoundChannel().send("endRoundViaFlip", getAllUserIdsOfGameTeams(game.getTeams()));
-                	}
-            	}    
+            if (game.getStatus().equals(GameState.PLAYED)) {   // checks if game is in state PLAYED otherwise there should not be started a new round
+                if (!midValidation.get(game)) {
+                    if (!activeRound.get(game)) {
+                        activeRound.put(game, true);
+                        startRoundByCube(game, currentRound.get(game), configuredFacetsEvent.getCubeFace());
+                    }
+                    else {
+                        activeRound.put(game, false);
+                        midValidation.put(game, true);
+                        sendGameMessage(game, "endRoundViaFlip");
+                    }
+                }
             }
         }
     }
 
     /**
-     * A method for processing a {@link ChannelPresenceEvent}. Checks for each game currently to be played whether
-     * enough players are present and sets the games state accordingly. Is called by a channel supervisor when players
-     * enter or leave the game room.
+     * A method for processing a {@link ChannelPresenceEvent}.
+     * Checks for each game currently to be played whether enough players are present and sets the games state accordingly.
+     * Is called by a channel supervisor when players enter or leave the game room.
      * @apiNote Game should be saved here, but cannot for missing authentication context.
      */
     public synchronized void onChannelPresenceEvent(ChannelPresenceEvent channelPresenceEvent) {
@@ -115,57 +116,73 @@ public class GameManagerController {
 
         final Set<Long> userIds = channelPresenceEvent.getUserIds();
         for (Game game : this.listOfGames.values()) {
+            // count present teams
             Set<Team> teams = game.getTeams();
             int countTeams = teams.size();
             int counter = 0;
             for (Team team : teams) {
                 if (team.getTeamMembers().stream().anyMatch(u -> userIds.contains(u.getId()))) counter++;
             }
+            presentTeams.put(game, counter);
+
+            // switch state if appropriate
             GameState preStatus = game.getStatus();
-            game.setStatus(counter < countTeams
-                    ? game.getStatus() == GameState.PLAYED ? GameState.HALTED : GameState.VALID_SETUP
-                    : GameState.PLAYED);
-            if((preStatus==GameState.VALID_SETUP) && (game.getStatus() == GameState.PLAYED) && !currentRound.containsKey(game)) {
-            	getNextRoundInfo(game);
-            	this.websocketManager.getNewRoundChannel().send("startGame", getAllUserIdsOfGameTeams(game.getTeams()));
+            if (preStatus == GameState.VALID_SETUP || preStatus == GameState.PLAYED) {
+                game.setStatus(counter < countTeams ? GameState.VALID_SETUP : GameState.PLAYED);
             }
-            if((game.getStatus() == GameState.PLAYED) && (preStatus == GameState.HALTED)) {
-            	this.websocketManager.getNewRoundChannel().send("restartGame", getAllUserIdsOfGameTeams(game.getTeams()));
+            
+            // TODO: save game to keep db up-to-date
+            // game = saveGame(game);
+
+            // act according to current state
+            switch (game.getStatus()) {
+                case PLAYED:
+                    switch (preStatus) {
+                        case PLAYED:
+                            sendGameMessage(game, isGameMidRound(game) ? "setup" : "setupandupdate");
+                            break;
+
+                        default:
+                            if (currentRound.containsKey(game)) {
+                                sendGameMessage(game, "restartGame");
+                            }
+                            else {
+                                getNextRoundInfo(game);
+                                sendGameMessage(game, "startGame");
+                            }
+                            break;
+                    }
+                    break;
+
+                case CANCELED:
+                    endGame(game, false);
+                    sendGameMessage(game, "gameCanceled");
+                    break;
+                    
+                default:
+                    sendGameMessage(game, "pauseGame");
+                    break;
             }
-            if((game.getStatus() == GameState.PLAYED) && (preStatus == GameState.PLAYED)) {
-            	if(isGameMidRound(game)) {
-            		this.websocketManager.getNewRoundChannel().send("setup", getAllUserIdsOfGameTeams(game.getTeams()));
-            	} else {
-            		this.websocketManager.getNewRoundChannel().send("setupandupdate", getAllUserIdsOfGameTeams(game.getTeams()));
-            	}	
-            }
-            if(game.getStatus() == GameState.HALTED) {
-            	this.websocketManager.getNewRoundChannel().send("pauseGame", getAllUserIdsOfGameTeams(game.getTeams()));
-            }
-            // should, but cannot save game here, because authentication is missing from context (event was externally
-            // initialized)
-            // TODO: save game on next occasion to keep db up-to-date
-            // gameService.saveGame(game);
         }
     }
-    
+
     /**
-     * Method that starts a new Round by the cube for a game. It initializes a new round and also calls a method to start the
-     * countdown.
+     * Method that starts a new Round by the cube for a game.
+     * It initializes a new round and also calls a method to start the countdown.
      * @param currentGame, game for which round should be started
-     * @param cubeFace,    face that sets round parameter
+     * @param cubeFace face that sets round parameter
      */
     public void startRoundByCube(Game game, Round round, CubeFace cubeFace) {
         this.currentRound.put(game, gameLogic.getCubeInfosIntoRound(round, cubeFace));
-        this.websocketManager.getNewRoundChannel().send("startRound", getAllUserIdsOfGameTeams(game.getTeams()));
+        sendGameMessage(game, "startRound");
     }
-    
+
     /**
      * Method to get Informations of next round, before cube starts the round
-     * @param game, game for which informations of next round should be estimated
+     * @param game game for which informations of next round should be estimated
      */
     public void getNextRoundInfo(Game game) {
-    	this.currentRound.put(game, gameLogic.getNextRound(game));
+        this.currentRound.put(game, gameLogic.getNextRound(game));
     }
 
     public Round getCurrentRoundOfGame(Game game) {
@@ -174,7 +191,7 @@ public class GameManagerController {
 
     /**
      * method to get the current round for a given user, called by {@link UserGameController}
-     * @param user to find current round
+     * @param  user to find current round
      * @return current round
      */
     public Round getCurrentRoundForUser(User user) {
@@ -183,6 +200,7 @@ public class GameManagerController {
             for (Team t : e.getKey().getTeams()) {
                 if (t.getTeamMembers().contains(user)) {
                     round = e.getValue();
+                    break;
                 }
             }
         }
@@ -191,7 +209,7 @@ public class GameManagerController {
 
     /**
      * A method to put all usernames of players, that are online, into a list
-     * @param listOfTeams
+     * @param  listOfTeams
      * @return list of usernames
      */
     private List<Long> getAllUserIdsOfGameTeams(Set<Team> listOfTeams) {
@@ -206,7 +224,7 @@ public class GameManagerController {
 
     /**
      * find the game in which a given user is currently playing, called by{@link CouuntDownController}
-     * @param user to find current game
+     * @param  user to find current game
      * @return current game
      */
     public Game getCurrentGameForUser(User user) {
@@ -240,42 +258,44 @@ public class GameManagerController {
 
             // send invitation to all contained team members
             Set<Long> invited = game.getTeams().stream().flatMap(t -> t.getTeamMembers().stream().map(User::getId))
-                    .collect(Collectors.toSet());
+                .collect(Collectors.toSet());
             websocketManager.getMessageChannel()
-                    .send(Map.of("type", "gameInvitation", "name", game.getName(), "id", game.getId()), invited);
+                .send(Map.of("type", "gameInvitation", "name", game.getName(), "id", game.getId()), invited);
         }
     }
 
     /**
-     * Method to validate and save a round of a game. Checks if a team reached the maximum of points or if all enabled terms
-     * of topic have been used. Send corresponding message to all users in the game
+     * Method to validate and save a round of a game.
+     * Checks if a team reached the maximum of points or if all enabled terms of topic have been used.
+     * Send corresponding message to all users in the game
      * @param game
-     * @param v, validation of round
+     * @param v validation of round
      */
     public void validateRoundOfGame(Game game, Validation v) {
         gameLogic.saveLastRound(game, v);
         this.listOfGames.put(getCubeByGame(game), game);
         midValidation.put(game, false);
         if (gameLogic.teamReachedMaxPoints(game, this.currentRound.get(game).getGuessingTeam())) {
-            endGame(game);
-            this.websocketManager.getNewRoundChannel().send("gameOver", getAllUserIdsOfGameTeams(game.getTeams()));
-        } else { 
-        	if (!gameLogic.stillTermsAvailable(game)) {
-        		Team winningTeam = gameLogic.getTeamWithMostPoints(game);
+            endGame(game, true);
+            sendGameMessage(game, "gameOver");
+        }
+        else {
+            if (!gameLogic.stillTermsAvailable(game)) {
+                Team winningTeam = gameLogic.getTeamWithMostPoints(game);
                 game.setMaxPoints(roundService.getPointsOfTeamInGame(game, winningTeam));
-                endGame(game);
-                this.websocketManager.getNewRoundChannel().send("termsOver", getAllUserIdsOfGameTeams(game.getTeams()));
-        	} else {
-            	getNextRoundInfo(this.listOfGames.get(getCubeByGame(game)));
-                this.websocketManager.getNewRoundChannel().send("validatedRound", getAllUserIdsOfGameTeams(game.getTeams()));
+                endGame(game, true);
+                sendGameMessage(game, "termsOver");
             }
-
+            else {
+                getNextRoundInfo(this.listOfGames.get(getCubeByGame(game)));
+                sendGameMessage(game, "validatedRound");
+            }
         }
     }
 
     /**
      * Method to get the cube that corresponds to a game
-     * @param game, game to find cube of
+     * @param  game game to find cube of
      * @return cube of the game
      */
     private Cube getCubeByGame(Game game) {
@@ -290,85 +310,83 @@ public class GameManagerController {
     public void setActiveRoundFalse(Game game) {
         this.activeRound.put(game, false);
     }
-    
+
     public void setMidValidationTrue(Game game) {
-    	this.midValidation.put(game, true);
+        this.midValidation.put(game, true);
     }
 
     /**
-     * Method to end the game. Sets status to finished, saves it in the database and removes it from all
-     * maps in controller
-     * @param game, game that finishes
+     * Method to end the game. Sets status to finished, saves it in the database
+     * and removes it from all maps in controller
+     * @param game game that finishes
      */
-    public void endGame(Game game) {
-        game.setStatus(GameState.FINISHED);
-        gameService.saveGame(game);
-                
+    public void endGame(Game game, boolean finished) {
+        game.setStatus(finished ? GameState.FINISHED : GameState.CANCELED);
+        if (finished) gameService.saveGame(game);
+
         this.listOfGames.remove(getCubeByGame(game));
         this.currentRound.remove(game);
         this.activeRound.remove(game);
+        this.midValidation.remove(game);
+        this.presentTeams.remove(game);
     }
 
+    /**
+     * finds a game with a given cube
+     * @param  cube to find the game
+     * @return the game
+     */
+    public Game getCurrentGameForCube(Cube cube) {
+        return this.listOfGames.get(cube);
+    }
 
-	/**
-	 * finds a game with a given cube
-	 * 
-	 * @param cube to find the game
-	 * @return the game
-	 */
-	public Game getCurrentGameForCube(Cube cube) {
-		return this.listOfGames.get(cube);
-	}
-	
-	/**
-	 * called by {@link CubeStatusController} if there is a health status to report i.e. low battery, no connection
-	 * puts current game on halted if cube is OFFLINE
-	 * 
-	 * @param cube to to report to the current game
-	 */
-	public void healthNotification(Cube cube) {
+    /**
+     * called by {@link CubeStatusController} if there is a health status to report i.e. low battery,
+     * no connection puts current game on halted if cube is OFFLINE
+     * @param cube to report to the current game
+     */
+    public void healthNotification(Cube cube) {
+        Game game = getCurrentGameForCube(cube);
+        sendGameMessage(game, "healthMessage");
+    }
 
-		Game game = getCurrentGameForCube(cube);
+    /**
+     * called by {@link CubeStatusController} if Cube is offline -> GameStatus should be put to {@link GameState#VALID_SETUP}
+     * @param cube which is offline
+     */
+    public void cubeOffline(Cube cube) {
+        if (listOfGames.containsKey(cube)) {
+            Game game = listOfGames.get(cube);
+            if (game.getStatus() == GameState.PLAYED) game.setStatus(GameState.VALID_SETUP);
+        }
+    }
 
-		if(game!= null) {
-			List<Long> usersToNotify = getAllUserIdsOfGameTeams(game.getTeams());
-		
-		if(websocketManager != null) {
-			websocketManager.getNewRoundChannel().send("healthMessage", usersToNotify);
-			}
-		}
-	}
-	
-	/**
-	 * called by {@link CubeStatusController} if Cube is offline -> GameStatus should be put to HALTED
-	 * 
-	 * @param cube which is offline
-	 */
-	public void cubeOffline(Cube cube) {
-		if(listOfGames.containsKey(cube)) {
-			listOfGames.get(cube).setStatus(GameState.HALTED);
-		}
-	}
-	
-	/**
-	 * called by {@link CubeStatusController} if Cube is online -> GameStatus should be put to PLAYED
-	 * 
-	 * @param cube which is online
-	 */
-	public void cubeOnline(Cube cube) {
-		if(listOfGames.containsKey(cube)) {
-			listOfGames.get(cube).setStatus(GameState.PLAYED);
-		}
-	}
-	
+    /**
+     * called by {@link CubeStatusController} if Cube is online -> GameStatus should be put to PLAYED if otherwise appropriate
+     * @param cube which is online
+     */
+    public void cubeOnline(Cube cube) {
+        if (listOfGames.containsKey(cube)) {
+            Game game = listOfGames.get(cube);
+            if (game.getStatus() == GameState.VALID_SETUP && presentTeams.get(game) >= game.getTeamCount()) {
+                game.setStatus(GameState.PLAYED);
+            }
+        }
+    }
 
-	private boolean isGameMidRound(Game game) {
-		if(this.activeRound.get(game)) {
-			return true;
-		}
-		if(this.midValidation.get(game)) {
-			return true;
-		}
-		return false;
-	}
+    private boolean isGameMidRound(Game game) {
+        if (this.activeRound.get(game)) {
+            return true;
+        }
+        if (this.midValidation.get(game)) {
+            return true;
+        }
+        return false;
+    }
+
+    private void sendGameMessage(Game game, String message) {
+        if (game != null) {
+            this.websocketManager.getNewRoundChannel().send(message, getAllUserIdsOfGameTeams(game.getTeams()));
+        }
+    }
 }
